a4e7f83acb99867816e30b5edcc7f6ca57114411
Branislav Djordjevic <46406170+undefined@users.noreply.github.com>
2022-03-11 10:51:20 +0000

    Push from cutomization

---
+++ buttons/src/buttons.ts
@@ -0,0 +1,34 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const buttons = extendTheme({
+  components: {
+    Button: {
+      // 1. We can update the base styles
+      baseStyle: {},
+      // 2. We can add a new button size or extend existing
+      sizes: {
+        base: {
+          h: '56px',
+          fontSize: 'lg',
+          px: '3',
+        },
+      },
+      // 3. We can add a new visual variant
+      variants: {
+        primary: {
+          bg: 'yellow.400',
+        },
+        // 4. We can override existing variants
+        solid: (props) => ({
+          bg: props.colorMode === 'dark' ? 'red.300' : 'red.500',
+        }),
+      },
+      defaultProps: {
+        size: 'base',
+        variant: 'primary',
+      },
+    },
+  },
+});
+
+export default buttons;
